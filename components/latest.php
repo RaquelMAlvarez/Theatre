31894346edb261416e13e8b39aba5fc9000299bd
Your Name <you@example.com>
2023-05-02 12:16:55 +0100

    Edit images and content home,login,registre

--- components/latest.php
+++ components/latest.php
@@ -3,37 +3,37 @@
         <h2 class="text-2xl font-extrabold text-gray-900">BLOG</h2>
         <section class="mt-6 grid grid-cols-1 md:grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-8">
             <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl  transition duration-300 ease-in-out"
-                style="background-image: url('https://images.unsplash.com/photo-1623479322729-28b25c16b011?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80');">
+                style="background-image: url('<?= ROOT_DIR ?>assets/images/ballet-production-performance-don-quixote-45258.jpeg');">
                 <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
                 <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
                     <h3 class="text-center">
                         <a class="text-white text-2xl font-bold text-center" href="#">
                             <span class="absolute inset-0"></span>
-                            Top 10 highest paid programming languages of 2021
+                            TOP 10 UK THEATRE BLOGS
                         </a>
                     </h3>
                 </div>
             </article>
             <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl  transition duration-300 ease-in-out"
-                style="background-image: url('assets/images/grease_musical.jpg');">
+                style="background-image: url('<?= ROOT_DIR ?>assets/images/trapecio.jpeg');">
                 <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
                 <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
                     <h3 class="text-center">
                         <a class="text-white text-2xl font-bold text-center" href="#">
                             <span class="absolute inset-0"></span>
-                            Python Frameworks
+                            Best Theatre Blogs and Websites
                         </a>
                     </h3>
                 </div>
             </article>
             <article class="relative w-full h-64 bg-cover bg-center group rounded-lg overflow-hidden shadow-lg hover:shadow-2xl  transition duration-300 ease-in-out"
-                style="background-image: url('https://images.unsplash.com/photo-1511376777868-611b54f68947?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80');">
+                style="background-image: url('<?= ROOT_DIR ?>assets/images/moredance.jpg');">
                 <div class="absolute inset-0 bg-black bg-opacity-50 group-hover:opacity-75 transition duration-300 ease-in-out"></div>
                 <div class="relative w-full h-full px-4 sm:px-6 lg:px-4 flex justify-center items-center">
                     <h3 class="text-center">
                         <a class="text-white text-2xl font-bold text-center" href="#">
                             <span class="absolute inset-0"></span>
-                            The best plugins for Visual Studio Code
+                            Best UK Theatre Blogs to follow in 2022-2023
                         </a>
                     </h3>
                 </div>
